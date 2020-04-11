01e2802f9e2a47e277d8a5f3e34bf8f5a6997deb
ppnf <nanefathoummalt@gmail.com>
2020-04-11 14:25:20 +0700

    Hw-ep60
    File Money manager

---
+++ HW-ep60/HW-ep60/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HW_ep60
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_enter_Click(object sender, RoutedEventArgs e)
+        {
+            int income = int.Parse(txt_income.Text);
+            int pay = int.Parse(txt_pay.Text);
+            int need = int.Parse(txt_need.Text);
+            int day = need / (income - pay);
+            txt_day.Text = "" + day;
+        }
+    }
+}
